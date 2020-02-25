81bc46f4f4e1e11cd3336961dbbdd2e3f8085489
Eldfael <eddie.rayner1@gmail.com>
2020-02-25 13:04:34 +0000

    LevelNode data type Added

---
+++ Assets/Scripts/Level Generation/LevelNode.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum NodeType 
+{ 
+    NORMAL,ITEM,BOSS,START
+}
+public class LevelNode
+{
+    LevelNode parentNode;
+    List<LevelNode> childrenNodes;
+
+    NodeType nodeType;
+    int depth;
+
+    public LevelNode()
+    {
+        parentNode = null; // The starting room
+        nodeType = NodeType.START;
+
+        childrenNodes = new List<LevelNode>();
+        depth = 0;
+    }
+
+    public LevelNode(LevelNode _parentNode, NodeType _nodeType)
+    {
+        parentNode = _parentNode;
+        nodeType = _nodeType;
+
+        childrenNodes = new List<LevelNode>();
+        depth = parentNode.GetDepth() + 1;
+    }
+
+    public LevelNode GetParentNode()
+    {
+        return parentNode;
+    }
+    public LevelNode CreateChildNode(NodeType _nodeType)
+    {
+        LevelNode childNode = new LevelNode(this, _nodeType);
+        childrenNodes.Add(childNode);
+        return childNode;
+        
+    }
+    public LevelNode GetChildNode(int index)
+    {
+        return childrenNodes[index];
+    }
+    public int GetNumberOfChildren()
+    {
+        return childrenNodes.Capacity;
+    }
+    public NodeType GetNodeType()
+    {
+        return nodeType;
+    }
+    public void SetNodeType(NodeType _nodeType)
+    {
+        nodeType = _nodeType;
+    }
+    public int GetDepth()
+    {
+        return depth;
+    }
+}
---
+++ Assets/Scripts/Level Generation/LevelTree.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTree : MonoBehaviour
+{
+    LevelNode startNode;
+    LevelNode currentNode;
+    List<LevelNode> nodeList;
+    private void Start()
+    {
+        startNode = new LevelNode();
+        currentNode = startNode;
+
+        nodeList = new List<LevelNode>();
+        nodeList.Add(startNode);
+
+        // Creating the longest route to the boss room
+        int maxDepth = Random.Range(5, 8);
+        for (int x = 0; x < maxDepth; x++)
+        {   
+            if (x < maxDepth - 1)
+            {
+                currentNode = currentNode.CreateChildNode(NodeType.NORMAL);
+            }
+            else
+            {
+                currentNode = currentNode.CreateChildNode(NodeType.BOSS);
+            }
+            nodeList.Add(currentNode);
+        }
+        
+
+    }
+}
